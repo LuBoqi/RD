3a5e4864db514394a4f0e66a1487100f3791dcf5
BoqiLu <boqi_lu@petalmail.com>
2024-06-04 15:42:16 +0800

    add funny test thing

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ import mediapipe as mp
 
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
-mp_hands = mp.solutions.hands
+mp_holistic = mp.solutions.holistic
 device = Device()
 frames = {}
 color_image = []
@@ -26,22 +26,33 @@ def cap_mat():
 if __name__ == '__main__':
     _thread.start_new_thread(cap_mat, ())
     time.sleep(1)
-    with mp_hands.Hands(model_complexity=0, min_detection_confidence=0.5, min_tracking_confidence=0.5) as hands:
+    with mp_holistic.Holistic(
+            min_detection_confidence=0.5,
+            min_tracking_confidence=0.5) as holistic:
         while True:
             image = color_image
             # To improve performance, optionally mark the image as not writeable to
             # pass by reference.
             image.flags.writeable = False
             image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-            results = hands.process(image)
-            # Draw the hand annotations on the image.
+            results = holistic.process(image)
+
+            # Draw landmark annotation on the image.
             image.flags.writeable = True
             image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-            if results.multi_hand_landmarks:
-                for hand_landmarks in results.multi_hand_landmarks:
-                    mp_drawing.draw_landmarks(image, hand_landmarks, mp_hands.HAND_CONNECTIONS,
-                                              mp_drawing_styles.get_default_hand_landmarks_style(),
-                                              mp_drawing_styles.get_default_hand_connections_style())
+            mp_drawing.draw_landmarks(
+                image,
+                results.face_landmarks,
+                mp_holistic.FACEMESH_CONTOURS,
+                landmark_drawing_spec=None,
+                connection_drawing_spec=mp_drawing_styles
+                .get_default_face_mesh_contours_style())
+            mp_drawing.draw_landmarks(
+                image,
+                results.pose_landmarks,
+                mp_holistic.POSE_CONNECTIONS,
+                landmark_drawing_spec=mp_drawing_styles
+                .get_default_pose_landmarks_style())
             # Flip the image horizontally for a selfie-view display.
-            cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
+            cv2.imshow('MediaPipe Holistic', cv2.flip(image, 1))
             cv2.waitKey(1)
