f3afa90c89da5a0300f99f3a7c7d3b87627378ce
BoqiLu <boqi_lu@petalmail.com>
2024-06-04 15:33:13 +0800

    delete useless code

--- main.py
+++ main.py
@@ -23,46 +23,6 @@ def cap_mat():
                 color_image = color_frame.to_array()
 
 
-IMAGE_FILES = []
-with mp_hands.Hands(
-        static_image_mode=True,
-        max_num_hands=2,
-        min_detection_confidence=0.5) as hands:
-    for idx, file in enumerate(IMAGE_FILES):
-        # Read an image, flip it around y-axis for correct handedness output (see
-        # above).
-        image = cv2.flip(cv2.imread(file), 1)
-        # Convert the BGR image to RGB before processing.
-        results = hands.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-
-        # Print handedness and draw hand landmarks on the image.
-        print('Handedness:', results.multi_handedness)
-        if not results.multi_hand_landmarks:
-            continue
-        image_height, image_width, _ = image.shape
-        annotated_image = image.copy()
-        for hand_landmarks in results.multi_hand_landmarks:
-            print('hand_landmarks:', hand_landmarks)
-            print(
-                f'Index finger tip coordinates: (',
-                f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x * image_width}, '
-                f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * image_height})'
-            )
-            mp_drawing.draw_landmarks(
-                annotated_image,
-                hand_landmarks,
-                mp_hands.HAND_CONNECTIONS,
-                mp_drawing_styles.get_default_hand_landmarks_style(),
-                mp_drawing_styles.get_default_hand_connections_style())
-        cv2.imwrite(
-            '/tmp/annotated_image' + str(idx) + '.png', cv2.flip(annotated_image, 1))
-        # Draw hand world landmarks.
-        if not results.multi_hand_world_landmarks:
-            continue
-        for hand_world_landmarks in results.multi_hand_world_landmarks:
-            mp_drawing.plot_landmarks(
-                hand_world_landmarks, mp_hands.HAND_CONNECTIONS, azimuth=5)
-
 if __name__ == '__main__':
     _thread.start_new_thread(cap_mat, ())
     time.sleep(1)
