91c076b5ca6d5d5d5123ed7f64c8c7dc9d893bf2
BoqiLu <boqi_lu@petalmail.com>
2024-06-05 10:06:38 +0800

    add ros

---
+++ ros_ws/src/greedy_snake/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+package_name = 'greedy_snake'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='lbq',
+    maintainer_email='boqi_lu@petalmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
